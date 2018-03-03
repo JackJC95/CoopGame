a532d827968932e80f078bcf08088c14ade80ae7
JackJC95 <jj-mousey@hotmail.co.uk>
2018-03-03 21:24:55 +0000

    L68 Camera shake while firing

--- Source/CoopGame/Public/SWeapon.h
+++ Source/CoopGame/Public/SWeapon.h
@@ -46,5 +46,8 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 	UParticleSystem* TracerEffect;	
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	TSubclassOf<UCameraShake> FireCamShake;
 	
 };
