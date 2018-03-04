2d5e2c2240edc93e4ee26cb91782690b128625a4
JackJC95 <jj-mousey@hotmail.co.uk>
2018-03-04 12:24:22 +0000

    L82 Explosive barrel

---
+++ Source/CoopGame/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SExplosiveBarrel.h"
+#include "SHealthComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+	HealthComp = CreateDefaultSubobject<USHealthComponent>(TEXT("HealthComp"));
+	HealthComp->OnHealthChanged.AddDynamic(this, &ASExplosiveBarrel::OnHealthChanged);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetSimulatePhysics(true);
+	// Set to physics body to let radial component affect us (e.g. when a nearby barrel explodes)
+	MeshComp->SetCollisionObjectType(ECC_PhysicsBody);
+	RootComponent = MeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComp"));
+	RadialForceComp->SetupAttachment(MeshComp);
+	RadialForceComp->Radius = 250;
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->bAutoActivate = false;
+	RadialForceComp->bIgnoreOwningActor = true;
+
+	ExplosionImpulse = 400;
+}
+
+void ASExplosiveBarrel::OnHealthChanged(USHealthComponent * OwningHealthComp, float Health, float HealthDelta, const UDamageType * DamageType, AController * InstigatedBy, AActor * DamageCauser)
+{
+	if (bExploded)
+	{
+		return;
+	}
+
+	if (Health <= 0.0f)
+	{
+		bExploded = true;
+
+		FVector BoostIntensity = FVector::UpVector * ExplosionImpulse;
+		MeshComp->AddImpulse(BoostIntensity, NAME_None, true);
+
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+		MeshComp->SetMaterial(0, ExplodedMaterial);
+
+		RadialForceComp->FireImpulse();
+	}
+}
